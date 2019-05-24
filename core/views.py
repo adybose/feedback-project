f0d6a8930a8a62b0fabf44beb5af22ef58034d85
Aditya Bose <13054902+adbose@users.noreply.github.com>
2019-05-24 12:27:00 +0530

    Feedback view hack

--- core/views.py
+++ core/views.py
@@ -15,9 +15,13 @@ class RegistrationPageView(TemplateView):
     def post(self, request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
-            user = form.save()
             print("form is valid")
+            user = form.save()
+            print(user)
             print(request.POST)
+            user_dict = dict(form.data)
+            del user_dict['csrfmiddlewaretoken']
+            request.session['user_dict'] = user_dict
 
         else:
             print("form is invalid")
@@ -29,27 +33,47 @@ class FeedbackView(TemplateView):
     def get(self, request):
         # form = FeedbackForm()
         ques = Question.objects.all()
+        print("Printing all the questions from the database")
         print(ques)
+        print("Printing all the questions with options on the console")
+        print("##########")
+        for que in ques:
+            print(que.id)
+            print(que.question)
+            print(que.optiona)
+            print(que.optionb)
+            print(que.optionc)
+            print(que.optiond)
+            print("##########")
+
         return render(request, template_name='core/feedback.html', context={'ques': ques})
         # return render(request, template_name='core/feedback.html', context={'form': form, 'ques': ques})
 
     def post(self, request):
         form = FeedbackForm(request.POST)
+        ques = Question.objects.all()
+        user_dict = request.session['user_dict']
+        data_dict = dict(form.data)
+        response = {}
+        response["Full Name"] = user_dict["full_name"][0]
+        response["Email"] = user_dict["email"][0]
 
-        data = request.POST
-        data_dict = dict(data)
-        print(data_dict)
-        import json
-        data_json = json.dumps(data_dict)
-        print(data_json)
+        for key in data_dict.keys():
+            if key != 'csrfmiddlewaretoken':
+                response[str(ques[int(key)-1])] = data_dict[key][0]
 
-        if form.is_valid():
-            response = form.save()
-            print("form is valid")
-            print(response)
-
-        else:
-            print("form is invalid")
+        print(response)
+        # import json
+        # data_json = json.dumps(data_dict)
+        # print(data_json)
+        # import ipdb;ipdb.set_trace()
+        # if form.is_valid():
+        #     response = form.save()
+        #     print("form is valid")
+        #     print(response)
+        #
+        # else:
+        #     print("form is invalid")
         return render(request, template_name='core/feedback_submitted.html', context={'response': response})
 
 
