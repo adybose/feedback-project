341865f14995f7ca8f27e28d39007bd9a67f683e
Aditya Bose <13054902+adbose@users.noreply.github.com>
2019-05-24 19:44:00 +0530

    Feedback form view saving

--- core/views.py
+++ core/views.py
@@ -10,18 +10,36 @@ from .forms import UserRegistrationForm, FeedbackForm#, QuizForm
 class RegistrationPageView(TemplateView):
     def get(self, request):
         form = UserRegistrationForm()
+        print("Empty User Registration Form...")
+        print(form)
+
         return render(request, template_name='core/register.html', context={'form': form})
 
     def post(self, request):
         form = UserRegistrationForm(data=request.POST)
+        print("#####")
+        print("Registration form with data...")
+        print(form)
+        print("#####")
         if form.is_valid():
             print("form is valid")
             user = form.save()
+
+            print("#####")
+            print("User __str__")
             print(user)
+
+            print("#####")
+            print("Registration form Raw POST request ")
             print(request.POST)
+
             user_dict = dict(form.data)
             del user_dict['csrfmiddlewaretoken']
+            # saving the user dictionary to the session
             request.session['user_dict'] = user_dict
+            print("#####")
+            print("User dictionary saved in session without csrfmiddlewaretoken...")
+            print(user_dict)
 
         else:
             print("form is invalid")
@@ -33,7 +51,10 @@ class FeedbackView(TemplateView):
     def get(self, request):
         # form = FeedbackForm()
         ques = Question.objects.all()
-        print("Printing all the questions from the database")
+        # request.session['ques'] = ques
+        print("#####")
+        print("Raw printing all the questions from the database as a Queryset... (it's basically a list")
+        print("#####")
         print(ques)
         print("Printing all the questions with options on the console")
         print("##########")
@@ -50,22 +71,46 @@ class FeedbackView(TemplateView):
         # return render(request, template_name='core/feedback.html', context={'form': form, 'ques': ques})
 
     def post(self, request):
-        form = FeedbackForm(request.POST)
+        data = request.POST
+        print("#####")
+        print("Printing the raw feedback POST request as a Querydict...")
+        print(data)
+
+        # retrieving the questions from the session
+        # ques = request.session['ques']
+
+        print("#####")
         ques = Question.objects.all()
+        print("Printing the questions retrieved from the database as a Queryset...")
+        print(ques)
+
+        # retrieving the user from the session
         user_dict = request.session['user_dict']
-        data_dict = dict(form.data)
+        data_dict = dict(data)
+        del data_dict['csrfmiddlewaretoken']
+
+        print("#####")
+        print("Printing the response data as a dictionary without csrf middleware token")
+        print(data_dict)
+        print("#####")
+
         response = {}
         response["Full Name"] = user_dict["full_name"][0]
         response["Email"] = user_dict["email"][0]
 
+        qna_dict = {}
         for key in data_dict.keys():
             if key != 'csrfmiddlewaretoken':
-                response[str(ques[int(key)-1])] = data_dict[key][0]
+                qna_dict[str(ques[int(key)-1])] = data_dict[key][0]
 
+        response["Feedback"] = [qna_dict]
         print(response)
-        # import json
-        # data_json = json.dumps(data_dict)
-        # print(data_json)
+
+        import json
+        response_json = json.dumps(response)
+        print("#####")
+        print("Converting the response dictionary into JSON string")
+        print(response_json)
         # import ipdb;ipdb.set_trace()
         # if form.is_valid():
         #     response = form.save()
@@ -74,6 +119,9 @@ class FeedbackView(TemplateView):
         #
         # else:
         #     print("form is invalid")
+        f = open("feedback_response.json", "w")
+        f.write(response_json)
+        f.close()
         return render(request, template_name='core/feedback_submitted.html', context={'response': response})
 
 
